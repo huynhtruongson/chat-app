d1bd5eafc597f303d1914291123bb6022d45033c
AnhPhan49 <phanducanhkg@gmail.com>
2021-06-02 15:14:26 +0700

    edit file

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(cors())
 
 app.use(express.urlencoded({extended : false}))
 app.use(express.json())
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 5000
 
 app.get('/', (req,res) => {
     res.json({
