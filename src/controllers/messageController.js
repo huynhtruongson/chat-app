e498b7a9e117c758d8f66a77176963df8da77119
AnhPhan49 <phanducanhkg@gmail.com>
2021-06-24 22:48:43 +0700

    fix code

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -3,14 +3,10 @@ const messageModel = require("../models/messageModel")
 
 module.exports.getMessage = async (req, res) =>{
     try {
-        let {pageSkip} = req.query
+        let {pageSkip} = req.query.pageSkip*1 || 0
         let {receiver} = req.params
         if (!receiver) {
             throw new Error ("Missing receiver ID")
-        }
-
-        if(!parseInt(pageSkip)){
-            throw new Error ("Resquest is not a number type")
         }
 
         let countMessage = await messageModel.find({$or: [
