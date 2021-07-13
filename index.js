dd4b75fc263a15d98dcfc00389dc5eebb67f2b86
Sơn Huỳnh <htson.dev.it@gmail.com>
2021-07-13 22:24:50 +0700

    finish v1

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const io = require('socket.io')(server);
 
 const mongoConnect = require('./src/config/databaseConfig')
 const route = require('./src/routes')
-const {socketServer} = require('./socketServer')
+const {socketServer} = require('./socketServer');
+const path = require('path');
 
 app.use(cors())
 // app.set('socketio', io)
@@ -17,12 +18,16 @@ app.use(express.urlencoded({extended : false}))
 app.use(express.json())
 
 mongoConnect()
-
-route(app)
-
 io.on("connection", socket => {
     socketServer(socket)
 })
+route(app)
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+    app.get('*',(req,res) => {
+        res.sendFile(path.join(__dirname,'client','build','index.html'))
+    })
+}
 
 const port = process.env.PORT || 5000
 server.listen(port, () => {
