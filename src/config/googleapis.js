37a3a946bec8ac7a2fe007bed53c65e24bff02cb
Sơn Huỳnh <htson.dev.it@gmail.com>
2021-07-27 16:56:45 +0700

    fix google api

--- src/config/googleapis.js
+++ src/config/googleapis.js
@@ -6,7 +6,7 @@ const CLIENT_SECRET = process.env.GG_SECRET
 const REDIRECT_URI = process.env.REDIRECT_URI
 const REDIRECT_TOKEN = process.env.REDIRECT_TOKEN
 
-const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI)
+const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET,REDIRECT_TOKEN,REDIRECT_URI)
 oAuth2Client.setCredentials({ refresh_token: REDIRECT_TOKEN})
 
 module.exports = oAuth2Client
